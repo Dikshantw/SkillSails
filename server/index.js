953e8c90769a3cb0ea17127667b6b35bdf4eba28
Dikshantw <dikshantw11@gmail.com>
2024-02-15 01:56:21 +0530

    added databaseurl in .env

--- server/index.js
+++ server/index.js
@@ -1,30 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
-import { User, Admin, Course } from "./database.js";
-import { SECRET } from "./authentication.js";
-import { authenticatejwt } from "./authentication.js";
-import jwt from "jsonwebtoken";
 import cors from "cors";
 import adminRouter from "./admin.js";
 import userRouter from "./user.js";
+import dotenv from "dotenv";
 
 const app = express();
-const router = express.Router();
+dotenv.config().parsed;
 app.use(express.json());
 app.use(cors());
 app.use("/admin", adminRouter);
 app.use("/users", userRouter);
-const databaseUrl = import.meta.env.VITE_URL;
+const databaseUrl = process.env.MONGO_URI;
 
 mongoose
   .connect(databaseUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: "courses",
   })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Error connecting to MongoDB:", err));
+  .then(() => {
+    console.log("Connected to MongoDB");
 
-app.listen(3000, () => {
-  console.log("server running on 3000");
-});
+    app.listen(3000, () => {
+      console.log("server running on 3000");
+    });
+  })
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
